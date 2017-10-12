1737e703a93116421e20b0bc7f1c0ec30aa2d7dd
Yuri Thomas <yuri.tpinheiro@gmail.com>
2017-10-12 17:14:41 -0300

    Criação de constantes para sub diretórios
    Criação de constantes para manipulação simplificada de objetos.

--- setup.m
+++ setup.m
@@ -2,3 +2,10 @@ addpath(genpath(pwd));
 
 % TODO checar se o .mex já foi gerado
 mex -outdir s-function s-function/temexd_mod.c;
+
+root_dir = pwd;
+models_dir    = [root_dir, '/models/'];
+scripts_dir   = [root_dir, '/scripts/'];
+sfunction_dir = [root_dir, '/s-function/'];
+init_dir      = [root_dir, '/init/'];
+database_dir  = [root_dir, '/database/'];
