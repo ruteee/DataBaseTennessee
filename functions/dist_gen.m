ec8b0e6693e930b957146a68f960a0dcddd5003a
Yuri Thomas <yuri.tpinheiro@gmail.com>
2019-01-14 14:53:05 -0300

    Correction for dist gen
    When passing a time that is not present in the ts array the disturbance was not activated.
    Now the value for the ts used is the closest to the provided dist start and dist end.

--- functions/dist_gen.m
+++ functions/dist_gen.m
@@ -8,8 +8,8 @@ function [ dist ] = dist_gen( ts, dist_set )
     end
     
     for i = 1:length(dist_set.id)
-        start_ts = find(ts == dist_set.start(i));
-        end_ts = find(ts == dist_set.end(i));
+        [~, start_ts] = min(abs(ts - dist_set.start(i)));
+        [~, end_ts]= min(abs(ts - dist_set.end(i)));
         dist(start_ts:end_ts, dist_set.id(i) + 1) = ones(end_ts - start_ts + 1, 1);
     end
     
